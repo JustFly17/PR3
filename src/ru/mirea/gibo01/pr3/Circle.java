70ac0c6603dcf382a9fe70d4e694adc51e696ca7
JustFly <boom.exe001@gmail.com>
2021-09-19 18:48:04 +0300

    Initial commit

---
+++ src/ru/mirea/gibo01/pr3/Circle.java
@@ -0,0 +1,22 @@
+package ru.mirea.gibo01.pr3;
+
+public class Circle {
+
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getSquare(double radius) {
+        return Math.PI * Math.pow(radius,2);
+    }
+
+    public double getCircleLength(double radius) {
+        return Math.PI * radius * 2;
+    }
+}
